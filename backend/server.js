f6bde651abf99540d7edf03f13e83ea40e7efb86
b-l-u-e <winnie.gitau282@gmail.com>
2024-07-19 15:41:38 +0300

    added Dockerfile on frontend

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const cors = require('cors')
 const connectDB = require('./src/config/db');
+
+
 
 // Connect to Database
 connectDB();
 
 const app = express();
+app.use(cors())
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
